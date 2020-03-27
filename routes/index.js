89d2cf1a51efd56b2ae381e1fe1c41fb0ea5e55a
jiben22 <benji.legallo22@live.fr>
2020-03-27 11:56:12 +0100

    2nd commit

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const mountRegisterRoutes = require('../features/register/routes');
-const mountLoginRoutes = require('../features/login/routes');
-const mountLogoutRoutes = require('../features/logout/routes');
-const mountResetPasswordRoutes = require('../features/reset-password/routes');
-const mountProfileRoutes = require('../features/profile/routes');
-
 function isAuthenticated(req, res, next) {
-  if (req.user && req.isAuthenticated()) {
-    return next();
-  }
-
-  return res.redirect('/login');
+  return next();
 }
 
 /* GET home page. */
@@ -32,11 +22,5 @@ router.get('/maps', isAuthenticated, (req, res) => {
 router.get('/tables', isAuthenticated, (req, res) => {
   res.render('pages/tables');
 });
-
-mountRegisterRoutes(router);
-mountLoginRoutes(router);
-mountLogoutRoutes(router, [isAuthenticated]);
-mountResetPasswordRoutes(router);
-mountProfileRoutes(router, [isAuthenticated]);
 
 module.exports = router;
