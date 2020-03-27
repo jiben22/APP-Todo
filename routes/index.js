62890e6f8232114a5984e483c7e6ac8e73605116
jiben22 <benji.legallo22@live.fr>
2020-03-27 18:32:45 +0100

    Add todo and 404 pages

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-function isAuthenticated(req, res, next) {
-  return next();
-}
-
 /* GET home page. */
-router.get('/', isAuthenticated, (req, res) => {
+router.get('/', (req, res) => {
   res.render('pages/dashboard');
 });
 
-router.get('/icons', isAuthenticated, (req, res) => {
-  res.render('pages/icons');
-});
-
-router.get('/maps', isAuthenticated, (req, res) => {
-  res.render('pages/maps');
-});
-
-router.get('/tables', isAuthenticated, (req, res) => {
-  res.render('pages/tables');
+/* GET add todo page. */
+router.get('/todo', (req, res) => {
+  res.render('pages/addTodo');
 });
 
 module.exports = router;
