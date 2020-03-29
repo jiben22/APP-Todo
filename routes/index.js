86deb22ef24a0751a8736564a986eba482159071
jiben22 <benji.legallo22@live.fr>
2020-03-29 14:17:10 +0200

    Add todo adding and todo updating

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,21 @@ router.get('/', (req, res) => {
 });
 
 /* GET add todo page. */
-router.get('/todo', (req, res) => {
+router.get('/add-todo', (req, res) => {
   res.render('pages/addTodo');
+});
+/* GET update todo page. */
+router.get('/update-todo', (req, res) => {
+  res.render('pages/updateTodo');
+});
+
+/* GET add tag page. */
+router.get('/dd-tag', (req, res) => {
+  res.render('pages/addTag');
+});
+/* GET update tag page. */
+router.get('/update-tag', (req, res) => {
+  res.render('pages/updateTag');
 });
 
 module.exports = router;
